54ed94d60cec49aa1950ae6b6e23f9b2783240b3
Sean Robinson <sean.robinson@scottsdalecc.edu>
2021-09-02 13:49:36 -0700

    Add fake_proj send and receive
    Signed-off-by: Sean Robinson <sean.robinson@scottsdalecc.edu>

--- src/fake_proj.cpp
+++ src/fake_proj.cpp
@@ -21,9 +21,13 @@
 #include "bewield.h"
 #include "lineal.h"
 
+#include <iostream>
 #include <map>
 #include <memory>
+#include <random>
+#include <string>
 #include <termios.h>
+#include <thread>
 #include <utility>
 
 
@@ -38,7 +42,79 @@ std::map<const std::string, const std::string> responses {
 
 
 /* Constants to change runtime behavior. */
+constexpr bool debug { false };
 const std::string port { "port_b" };
+
+
+/* Returns the full protocol string received.
+ *
+ * Because recv is reading a virtual serial port, it does not do any error
+ * handling, including for incomplete messages.
+*/
+const std::string recv(Lineal &device) {
+    char buffer[ 32 ];
+
+    auto ret { device.readBytes(buffer, sizeof(buffer)) };
+
+    if ( ret < 0 ) {
+        std::cout << "Read failed for response" << std::endl;
+        throw std::runtime_error("Fatal error while reading from bewield.");
+    }
+    if ( debug && (ret > 0) ) {
+        std::cout << "read " << ret << " bytes" << std::endl;
+        std::string msg { buffer };
+        std::cout << cook(msg) << std::endl;
+    }
+
+    return std::string(buffer);
+}
+
+
+/* Sends a message to bewield through a virtual serial port and returns the
+ * quantity of sent bytes.
+ *
+ * To simulate a noisy serial line, send "transmits" partial random-length
+ * messages with random delays between "packets".
+ */
+std::size_t send(Lineal &device, const std::string response) {
+
+    // Random seed source
+    std::random_device rd;
+    // Random number generator
+    std::mt19937 gen(rd());
+    // Functions returning random ints in different ranges.
+    std::uniform_int_distribution<> random_delay { 20, 500 };
+    std::uniform_int_distribution<> random_size { 1, 20 };
+
+    std::size_t bytes_sent { 0 };
+    std::size_t start { 0 };
+
+    while ( start < response.length() ) {
+        std::chrono::duration<int, std::milli> delay { random_delay(gen) };
+        std::this_thread::sleep_for(delay);
+        if ( debug ) {
+            std::cout << "delayed: " << delay.count() << " ms" << std::endl;
+        }
+
+        auto random_length { random_size(gen) };
+        auto partial_msg { response.substr(start, random_length) };
+        start += random_length;
+
+        auto ret { device.write(partial_msg.c_str(), partial_msg.length()) };
+        if ( ret < 0 ) {
+            std::cout << "Write failed for '" << partial_msg << "'" << std::endl;
+            throw std::runtime_error("Fatal error while writing to bewield.");
+        } else {
+            bytes_sent += ret;
+        }
+        if ( debug ) {
+            std::cout << "wrote " << ret << " bytes" << std::endl;
+            std::cout << cook(partial_msg) << std::endl;
+        }
+    }
+
+    return bytes_sent;
+}
 
 
 int main(int argc, const char* argv[]) {
@@ -53,6 +129,33 @@ int main(int argc, const char* argv[]) {
 
     // Flush erroneous, pending IO before continuing.
     tcflush(serial->fd(), TCIOFLUSH);
+
+    while ( true ) {
+        std::string cmd { recv(*serial) };
+        auto start { cmd.rfind(PREFIX, std::string::npos) + 1 };
+        auto end { cmd.rfind(SUFFIX, std::string::npos) };
+        std::string cooked_cmd { cmd.substr(start, end-start) };
+
+        if ( debug ) {
+            std::cout << "cooked_cmd: " << cooked_cmd << std::endl;
+        }
+
+        std::string response;
+        try {
+            response = "  >*" + cooked_cmd + "#\r\r*" + responses.at(cooked_cmd) + "#\r";
+            if ( debug ) {
+                std::cout << "response: " << cook(response) << std::endl;
+            }
+        } catch ( const std::out_of_range &e ) {
+            continue;
+        }
+
+        if ( ! cmd.empty() ) {
+            if ( send(*serial, response) < response.length() ) {
+                throw std::runtime_error("Fake response not fully sent.");
+            }
+        }
+    }
 
     return EXIT_SUCCESS;
 }
